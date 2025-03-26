62c89b2d74ce8f53efedf90214d4dde1789660aa
debora23-07 <debmolive2@gmail.com>
2025-03-26 18:16:08 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,11 +2,12 @@
 
 int main() {
 
+    //colocando o nome das variaveis dos paises
 int Populacao;
     float Area;
     float PIB;
     int Turismo;
-
+//utilizando printf e scanf para o usuario digitar o que é necessário 
     printf("Digite a populacao do Brasil: \n");
     scanf("%d", &Populacao);
 
@@ -35,7 +36,7 @@ int Populacao;
 
     printf("Digite um ponto turistico da Espanha: \n");
     scanf("%d", &TURISMO);
-    
+    //utilizando printf com as variaveis diferentes para poder mostrar em ordem o que foi digitado pelo usuário
     printf("Populacao do Brasil: %d - Area do Brasil: %f - PIB do Brasil: %f - Ponto turistico do Brasil: %d \n", Populacao, Area, PIB, Turismo);
     printf("Populacao da Espanha: %d - Area da Espanha: %f - PIB da Espanha: %f - Ponto turistico da Espanha: %d \n", POPULACAO, AREA, Pib, TURISMO);
 
